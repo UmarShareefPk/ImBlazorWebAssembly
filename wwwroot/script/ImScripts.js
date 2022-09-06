2801786735e5ca5684df5d03ca09a5fc2d748a6b
UmarShareefPk <76015950+UmarShareefPk@users.noreply.github.com>
2022-09-07 02:56:46 +0500

    Color changes and New Incident

--- wwwroot/script/ImScripts.js
+++ wwwroot/script/ImScripts.js
@@ -58,53 +58,53 @@ window.drawOverallChart = (data) => {
                 },
                 showInLegend: true
             }
-        },
-        series: [{
+          },
+       series: [{
             name: 'Share',
             data: [                
                 {
                     name: 'New', y: data.new, color: {
-                        linearGradient: [0, 0, 0, 300],
+                        radialGradient: [0, 0, 0, 300],
                         stops: [
-                            [0, 'rgba(245,124,0,1)'],
-                            [1, 'rgba(255,226,0,1)']
+                            [0, 'rgba(222, 184, 68, 1)'],
+                            [1, 'rgba(250, 196, 19, 1)']
                         ]
                     }
                 },          
                 {
                     name: 'In Progress', y: data.inProgress, color: {
-                        linearGradient: [0, 0, 0, 300],
+                        radialGradient: [0, 0, 0, 300],
                         stops: [
-                            [0, 'rgba(25,118,210,1)'],
-                            [1, 'rgba(0,237,255,1)']
+                            [0, 'rgba(113, 166, 247, 1)'],
+                            [1, 'rgba(48, 128, 204, 1)']
                         ]
                     }
                 },
                 {
                     name: 'Closed', y: data.closed, color: {
-                        linearGradient: [0, 0, 0, 300],
+                        radialGradient: [0, 0, 0, 300],
                         stops: [
-                            [0, 'rgba(67,160,71,1)'],
-                            [1, 'rgba(0,255,187,1)']
+                            [0, 'rgba(66, 194, 56, 1)'],
+                            [1, 'rgba(113, 200, 120, 1)']
                         ]
                     }
                 },
           
                 {
                     name: 'Approved', y: data.approved, color: {
-                        linearGradient: [0, 0, 0, 300],
+                        radialGradient: [0, 0, 0, 300],
                         stops: [
-                            [0, 'rgba(27,94,32,1)'],
-                            [1, 'rgba(19,255,0,1)']
+                            [0, 'rgba(12, 99, 5, 1)'],
+                            [1, 'rgba(14, 131, 22, 1)']
                         ]
                     }
                 },
                 {
                     name: 'Late', y: data.late, color: {
-                        linearGradient: [0, 0, 0, 300],
+                        radialGradient: [0, 0, 0, 300],
                         stops: [
-                            [0, 'rgba(183,28,28,1)'],
-                            [1, 'rgba(255,140,0,1)']
+                            [0, 'rgba(218, 30, 28, 1)'],
+                            [1, 'rgba(131, 55, 14, 1)']
                         ]
                     }
                 }
@@ -122,46 +122,46 @@ window.drawMostAssignedToUserChart = (data) => {
     data = [       
         {
             name: data[0].Name, y: parseInt(data[0].Count), color: {
-                linearGradient: [0, 0, 0, 300],
+                radialGradient: [0, 0, 0, 300],
                 stops: [
-                    [0, 'rgba(255,114,0,1)'],
-                    [1, 'rgba(183,28,28,1)']
+                    [0, 'rgba(210,128,255,1)'],
+                    [1, 'rgba(141,46,194,1)']
                 ]
             }
         },
         {
             name: data[1].Name, y: parseInt(data[1].Count), color: {
-                linearGradient: [0, 0, 0, 300],
+                radialGradient: [0, 0, 0, 300],
                 stops: [
-                    [0, 'rgba(255,127,0,1)'],
-                    [1, 'rgba(229,57,53,1)']
+                    [0, 'rgba(210,128,255,1)'],
+                    [1, 'rgba(141,46,194,1)']
                 ]
             }
         },
         {
             name: data[2].Name, y: parseInt(data[2].Count), color: {
-                linearGradient: [0, 0, 0, 300],
+                radialGradient: [0, 0, 0, 300],
                 stops: [
-                    [0, 'rgba(255,127,0,1)'],
-                    [1, 'rgba(239,83,80,1)']
+                    [0, 'rgba(210,128,255,1)'],
+                    [1, 'rgba(141,46,194,1)']
                 ]
             }
         },
         {
             name: data[3].Name, y: parseInt(data[3].Count), color: {
-                linearGradient: [0, 0, 0, 300],
+                radialGradient: [0, 0, 0, 300],
                 stops: [
-                    [0, 'rgba(230,255,46,1)'],
-                    [1, 'rgba(160,157,53,1)']
+                    [0, 'rgba(210,128,255,1)'],
+                    [1, 'rgba(141,46,194,1)']
                 ]
             }
         },
         {
             name: data[4].Name, y: parseInt(data[4].Count), color: {
-                linearGradient: [0, 0, 0, 300],
+                radialGradient: [0, 0, 0, 300],
                 stops: [
-                    [0, 'rgba(104,255,38,1)'],
-                    [1, 'rgba(46,139,44,1)']
+                    [0, 'rgba(210,128,255,1)'],
+                    [1, 'rgba(141,46,194,1)']
                 ]
             }
         }
@@ -235,6 +235,44 @@ window.downloadCommentFile = (baseUrl, incidentId, file) => {
         "&contentType=" + file.contentType
     );
     return "Ok";
+}
+
+window.addIncident = async (baseUrl, fields) => {
+
+    let files = $("#" + fields.files).prop('files');
+    console.log(fields);
+    console.log(files);
+    
+    //const formData = new FormData();
+
+    //if (files) {
+    //    for (let i = 0; i < files.length; i++) {
+    //        formData.append(
+    //            "Attachment" + i + 1,
+    //            files[i],
+    //            files[i].name
+    //        );
+    //    }
+    //}
+    //formData.append("CommentText", commentText.trim());
+    //formData.append("IncidentId", incidentId);
+    //formData.append("UserId", userId);
+
+    //let url = baseUrl + "/Incidents/AddComment";
+
+    //let res = await fetch(url, {
+    //    method: 'post',
+    //    headers: new Headers({ 'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("token")) }),
+    //    body: formData,
+    //})
+    //    .then(res => res.json()).then(res => {
+    //        $("#" + commentFilesId).val(null);
+    //        $("#" + commentFilesId).replaceWith($("#" + commentFilesId).clone(true));
+    //        $("#commentFileuploadInfo").html("Click here to upload files");
+    //        return res;
+    //    }).catch(err => console.log(err));
+
+    return res;
 }
 
 window.addComment = async (baseUrl,commentText, commentFilesId, incidentId, userId) => {
